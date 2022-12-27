63a76da6a7a4a674db6e27fbba2eab2cb043d6ea
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2022-12-27 19:22:25 +0200

    change state to obj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,48 +5,41 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 
 export default function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
-  function countTotalFeedback() {
-    return good + neutral + bad;
-  }
+  const countTotalFeedback = () => {
+    return Object.values(feedback).reduce((acc, el) => acc + el);
+  };
 
-  function countPositiveFeedbackPercentage() {
-    return ((good / countTotalFeedback()) * 100).toFixed();
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return ((feedback.good / countTotalFeedback()) * 100).toFixed();
+  };
 
   function onLeaveFeedback(option) {
-    switch (option) {
-      case 'good':
-        setGood(prev => prev + 1);
-        break;
-      case 'neutral':
-        setNeutral(prev => prev + 1);
-        break;
-      case 'bad':
-        setBad(prev => prev + 1);
-        break;
-      default:
-        break;
-    }
+    setFeedback(prev => ({
+      ...prev,
+      [option]: prev[option] + 1,
+    }));
   }
 
   return (
     <>
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
+          options={Object.keys(feedback)}
           onLeaveFeedback={onLeaveFeedback}
         />
       </Section>
       <Section title="Statistics">
-        {countTotalFeedback() ? (
+        {countTotalFeedback() > 0 ? (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
